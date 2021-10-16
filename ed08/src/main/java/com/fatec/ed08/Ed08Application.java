a1c068a91b41c4fede692cf70fde511d752601c2
Rogério <rogrlj@hotmail.com>
2021-10-15 21:01:15 -0300

    Commit do exercicio ED08

---
+++ ed08/src/main/java/com/fatec/ed08/Ed08Application.java
@@ -0,0 +1,13 @@
+package com.fatec.ed08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ed08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ed08Application.class, args);
+	}
+
+}
